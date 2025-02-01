078140973a89c488bf17b9758131aa2964435354
GustavoNeery <gustavoalvesneryy@hotmail.com>
2025-01-31 21:15:00 -0300

    test: create test to save actor

---
+++ src/main/java/gustavoneery/libraryapi/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package gustavoneery.libraryapi.repository;
+
+import gustavoneery.libraryapi.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+}
